2eb13d47d95b6d11026d672297351aa0b0cf8c3c
Tridex_Claude <claude.compere@tridexcreative.com>
2016-01-28 22:56:01 +0000

    add update and delete methods

--- server.js
+++ server.js
@@ -1,3 +1,5 @@
+// CRUD Create Read Update Delete - Todo Api
+
 var express     = require('express');
 var bodyParser  = require('body-parser');
 var _           = require('underscore');
@@ -15,22 +17,15 @@ app.get('/', function(req, res){
   res.send("Working just fine");
 });
 
-// Get all the todo tasks
+// GET all the todo tasks
 app.get('/todos', function(req, res){
   res.json(todos);
 });
 
-// Get specific todo task
+// GET specific todo task
 app.get('/todos/:id', function(req, res){
-  var todoId = parseInt(req.params.id, 10);
+  var todoId      = parseInt(req.params.id, 10);
   var matchedTodo = _.findWhere(todos, {id: todoId});
-
-  // var matchedTodo;
-  // todos.forEach(function(todo){
-  //   if (todoId === todo.id){
-  //     matchedTodo = todo;
-  //   }
-  // });
 
   if (matchedTodo){
     res.json(matchedTodo);
@@ -38,15 +33,17 @@ app.get('/todos/:id', function(req, res){
     res.status(404).send();
   }
 
-  //res.send('Checking for todo id: ' + req.params.id);
 });
 
+// POST new todo taks
 app.post('/todos', function(req, res){
-  var body = req.body;
+  var body = _.pick(req.body, 'description', 'completed');
 
   if(!_.isBoolean(body.completed) || !_.isString(body.description) || body.description.trim().length === 0){
     return res.status(400).send();
   }
+
+  body.description = body.description.trim();
 
   body.id = todoNextId;
 
@@ -55,6 +52,49 @@ app.post('/todos', function(req, res){
 
   res.json(body);
 });
+
+// DELETE todo task
+app.delete('/todos/:id', function(req, res){
+  var todoId      = parseInt(req.params.id, 10);
+  var matchedTodo = _.findWhere(todos, {id: todoId});
+
+  if (!matchedTodo){
+    res.status(404).json({"error": "no todo task found with Id: " + todoId + ""});
+  } else {
+    todos = _.without(todos, matchedTodo);
+    res.json(matchedTodo);
+  }
+
+});
+
+// DELETE todo task
+app.put('/todos/:id', function(req, res){
+  var todoId      = parseInt(req.params.id, 10);
+  var matchedTodo = _.findWhere(todos, {id: todoId});
+  var body        = _.pick(req.body, 'description', 'completed');
+  var validAttributes = {};
+
+  if (!matchedTodo){
+    return res.status(404).send();
+  }
+
+  if (body.hasOwnProperty('completed') && _.isBoolean(body.completed)){
+    validAttributes.completed = body.completed;
+  } else if (body.hasOwnProperty('completed')){
+    return res.status(404).send();
+  }
+
+  if (body.hasOwnProperty('description') && _.isString(body.description) && body.description.trim().length > 0){
+    validAttributes.description= body.description;
+  } else if (body.hasOwnProperty('description')){
+    return res.status(400).send();
+  }
+
+  _.extend(matchedTodo, validAttributes);
+  res.json(matchedTodo);
+
+});
+
 
 app.listen(PORT, function(){
   console.log('Express listening on port: ' + PORT);
